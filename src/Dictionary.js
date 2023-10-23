bb8a67785442dca9f4f6a6dd4242d6a50c11671d
Megan-Woolhouse <146077608+Megan-Woolhouse@users.noreply.github.com>
2023-10-23 17:05:16 +0100

    added axios and API call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 export default function Dictionary() {
   let [word, setWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function capture(event) {
