3d0d62750fef9e690d91e8d01ddcd1cc204175b5
Megan-Woolhouse <146077608+Megan-Woolhouse@users.noreply.github.com>
2023-10-23 16:49:07 +0100

    Created react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+
+      <footer>
+        {" "}
+        <small>
+          Coded by Megan Woolhouse. Open-sourced on <a href="#">Github</a>
+        </small>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
